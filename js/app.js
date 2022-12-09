65df21e1dae964d59c183c685d50e3bb28cac2b0
Juan Garcia <juanmgarcia098@gmail.com>
2022-12-09 18:54:17 -0400

    Fixed some issues

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 const inputElement = document.querySelector("#cool")
 const buttonElement = document.querySelector("#submit-button")
 const ulElement = document.querySelector("todo-list")
+const inputEl = document.querySelector("input")
 
 
 buttonElement.addEventListener("click", function(evt) {
     const li = document.createElement("li")
-    const inputEl = document.querySelector("input")
     li.textContent = inputEl.value
     document.querySelector("ul")
     const ulEl = document.querySelector('ul')
     ulEl.appendChild(li)
+    inputEl.value = ''
 })
